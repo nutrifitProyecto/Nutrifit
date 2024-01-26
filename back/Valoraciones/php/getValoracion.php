51a1005c6d3680aa7fa718c84c80f0a59679da17
cesiouvas <sergiocastillorens@gmail.com>
2024-01-26 10:15:47 +0100

    query con left join

--- back/Valoraciones/php/getValoracion.php
+++ back/Valoraciones/php/getValoracion.php
@@ -5,7 +5,13 @@ include "../../inc/dbinfo.inc";
 //Creación de la conexión
 $connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
-$query = 'SELECT * FROM valoracion';
+//Consulta con legt join para obtener el nombre y apellidos de los entrenadores y clientes
+$query = 'SELECT valoracion.id, clientes.name AS cliName, clientes.surname AS cliSurname, entrenadores.name AS entName, entrenadores.surname AS entSurname, valoracion.rate
+ FROM valoracion
+ LEFT JOIN clientes
+ ON valoracion.idCliente = clientes.id
+ LEFT JOIN entrenadores
+ ON valoracion.idEntrenador = entrenadores.id';
 
 $result = $connection->query($query);
 
