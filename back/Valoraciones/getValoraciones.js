9763c52cc9054758013e704fd98565e3433e679f
cesiouvas <sergiocastillorens@gmail.com>
2024-01-26 10:37:08 +0100

    editar valoraciones rellenando selects con el nombre de los entrenadores y clientes

--- back/Valoraciones/getValoraciones.js
+++ back/Valoraciones/getValoraciones.js
@@ -35,26 +35,45 @@ function tablaValoraciones() {
                     <td>${rate.rate}</td>
                     <td>
                         <button onclick="eliminarEntrenador(${rate.id})" class="btn btn-danger">Eliminar</button>
-                        <button id="btnEditar" data-bs-toggle="modal" data-bs-target="#editarEntrenadorModal" class="btn btn-success" onclick="llenarCampos(${rate.id})">Editar</button>
+                        <button id="btnEditar" data-bs-toggle="modal" data-bs-target="#editarEntrenadorModal" class="btn btn-success" onclick="llenarSelectClientes()">Editar</button>
                     </td>
                 </tr>`
     });
     cad += `</table>
-                            <button data-bs-toggle="modal" data-bs-target="#crearEntrenadorModal" class="btn btn-primary">Crear entrenador</button>`
+                            <button data-bs-toggle="modal" data-bs-target="#crearValoracionModal" class="btn btn-primary">Crear entrenador</button>`
     table.innerHTML = cad
 }
 
-//Elimina un entrenador con respecto al id de este
+//Elimina una valoración con respecto al id de este
 function eliminarEntrenador(sendId) {
     confirm("Seguro que quieres eliminar al entrenador?")
     if (confirm) {
         $.ajax({
             type: "POST", //POST para enviar los datos al php
-            url: "./php/eliminarEntrenador.php",
+            url: "./php/eliminarValoracion.php",
             data: { idToDelete: sendId }, // Enviar la variable como parte de los datos
-            success: window.location = "./entrenadorList.html"
+            success: window.location = "./valoracionList.html"
         });
     }
+}
+
+function llenarSelectClientes() {
+    fetch('../Entrenadores/php/getEntrenador.php')
+    .then(response => response.json())
+    .then((data) => {
+        //Parsea la respuesta a JSON
+        info = JSON.parse(JSON.stringify(data));
+        //Llama a la función y crea una tabla con los entrenadores
+        let cad = ``
+        info.forEach(element => {
+            //IMPLEMENTAR IF PARA SABER AL QUE SE VA A EDITAR
+            cad += `<option value="${element.id}">${element.name} ${element.surname}</option>`
+        });
+        document.getElementById('rateEditEntrenador').innerHTML = cad
+    })
+    .catch(error => {
+        console.log(error);
+    });
 }
 
 //Rellena los campos de la ventana modal
