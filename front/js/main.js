441348a67b0c278894636edec7f94ed12a46ac28
aris7ides <joseamaristam@gmail.com>
2024-01-30 12:58:55 +0100

    añadido de campos al registerpage mediante js

--- front/js/main.js
+++ front/js/main.js
@@ -1,3 +1,4 @@
+//Boton de menu en movil
 function displayChange(){
     let bttn=document.querySelector("header nav");    
     if(bttn.style.display==="none"){
@@ -24,8 +25,37 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+/** */
+/*Boton de "soy entrenador" en el registerpage cuando se pulse se
+actualizara el formulario añadiendo unos inputs de experiencia y quittando otros*/
+function registerCoach(){
+  let div=document.getElementsByClassName("campoderegistro")[0];
+  while(div.hasChildNodes()){
+    div.firstChild.remove();    
+  }
+  let form=document.getElementsByTagName("form")[0];
+  let newinputs=
+  `<label for="rg-descripcion">Descripcion</label><textarea name="descripcion" id="rg-descripcion"></textarea>
+  <label for="rg-foto">Foto de Perfil</label><input type="file" id="rg-foto" name="foto" accept=".png, .jpg, .jpeg">
+  <label for="rg-ncurso">Nombre del Curso</label><input type="text" id="rg-ncurso" name="ncurso">
+  <label for="rg-descurso">Descripcion del Curso</label><textarea name="descurso" id="rg-descurso"></textarea>
+  <label>Etiquetas</label>
+  <div class="checkboxing">
+  <input type="checkbox" id="" name=""><label for="">ENTRENAMIENTO PERSONALIZADO</label>
+  <input type="checkbox" id="" name=""><label for="">MANTENIMIENTO INTEGRAL</label>
+  <input type="checkbox" id="" name=""><label for="">ASESORAMIENTO ALIMENTICIO</label>
+  <input type="checkbox" id="" name=""><label for="">SEGUIMIENTO CONTINUO Y AJUSTES</label>
+  <input type="checkbox" id="" name=""><label for="">PERDIDA DE PESO</label>
+  <input type="checkbox" id="" name=""><label for="">AUMENTO DE VOLUMEN</label>
+  </div>`;
 
+  form.getElementsByTagName("p")[0].textContent="";
 
+  div.innerHTML=newinputs;
+
+}
+
+/*
 //SUBIR CV
 
 
@@ -40,3 +70,4 @@ for (i = 0; i < acc.length; i++) {
          // Muestra el nombre del archivo seleccionado en el input de texto
          fileNameInput.value = fileInput.files[0].name;
      });
+*/
