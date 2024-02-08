0d9b2b6d78ac167e3f4b8f4080e3d18c0e2b1c7f
cesiouvas <sergiocastillorens@gmail.com>
2024-02-08 08:47:22 +0100

    logo redirige a index

--- front/js/main.js
+++ front/js/main.js
@@ -1,12 +1,13 @@
 //header
-window.addEventListener("load",function(){
-  let header=document.getElementsByTagName("header")[0];
-  header.innerHTML=`
-  <a href=".<img src="../img/bars.svg" alt="bars" class="bars-svg" onclick="displayChange()">
-  <div id="logo-title">
+window.addEventListener("load", function () {
+  let header = document.getElementsByTagName("header")[0];
+  header.innerHTML = `
+  <a href="./index.html">
+    <div id="logo-title" style="display: flex">
       <img src="../img/logo1.png" alt="logo" class="img-logo">
       <span id="title">NUTRI<span>FIT</span></span>
-  </div>
+    </div>
+  </a>
   <nav>
       <ul>
           <li><a href="./index.html">Servicios</a></li>
@@ -18,13 +19,13 @@ window.addEventListener("load",function(){
 });
 
 //Boton de menu en movil
-function displayChange(){
-    let bttn=document.querySelector("header nav");    
-    if(bttn.style.display==="none"){
-        bttn.style.display="block";
-    }else{
-        bttn.style.display="none";
-    }
+function displayChange() {
+  let bttn = document.querySelector("header nav");
+  if (bttn.style.display === "none") {
+    bttn.style.display = "block";
+  } else {
+    bttn.style.display = "none";
+  }
 }
 
 /** */
@@ -33,7 +34,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
@@ -47,12 +48,12 @@ for (i = 0; i < acc.length; i++) {
 /** */
 /*Boton de "soy entrenador" en el registerpage cuando se pulse se
 actualizara el formulario añadiendo unos inputs de experiencia y quittando otros*/
-function registerCoach(){
-  let div=document.getElementsByClassName("campoderegistro")[0];
-  while(div.hasChildNodes()){
-    div.firstChild.remove();    
+function registerCoach() {
+  let div = document.getElementsByClassName("campoderegistro")[0];
+  while (div.hasChildNodes()) {
+    div.firstChild.remove();
   }
-  let form=document.getElementsByTagName("form")[0];
+  let form = document.getElementsByTagName("form")[0];
   // let newinputs=
   // `<label for="rg-descripcion">Descripcion</label><textarea name="descripcion" id="rg-descripcion"></textarea>
   // <label for="rg-foto">Foto de Perfil</label><input type="file" id="rg-foto" name="foto" accept=".png, .jpg, .jpeg">
@@ -68,7 +69,7 @@ function registerCoach(){
   // <input type="checkbox" id="" name=""><label for="">AUMENTO DE VOLUMEN</label>
   // </div>`;
 
-  form.getElementsByTagName("p")[0].textContent="";
+  form.getElementsByTagName("p")[0].textContent = "";
 
   // div.innerHTML=newinputs;
 
