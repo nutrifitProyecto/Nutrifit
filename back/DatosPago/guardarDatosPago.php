d02f6be977bf2a998d1b27af8110b632ede33d1d
sergio castillo <sergiocastillorens@gmail.com>
2024-02-08 19:08:04 +0100

    vista datos de pago

---
+++ back/DatosPago/guardarDatosPago.php
@@ -0,0 +1,28 @@
+<?php
+include "../../inc/dbinfo.inc";
+
+//Conectar con la base de datos
+$connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+$name = $_POST['cliName'];
+$surname = $_POST['cliSurname'];
+$email = $_POST['cliEmail'];
+$passwd = $_POST['cliPasswd'];
+$tipo = $_POST['cliTipo'];
+$fecha = $_POST['cliFecha'];
+
+$hashContraseña = password_hash($passwd, PASSWORD_DEFAULT);
+
+$query = "INSERT INTO clientes (name, surname, email, password, tipo, fecha_nacimiento) VALUES ('$name', '$surname', '$email', '$hashContraseña', '$tipo', '$fecha')";
+
+$result = $connection->query($query);
+
+if ($result === true) {
+        header('Location: ../clientList.html');
+        exit();
+} else {
+        $query = "DELETE FROM clientes WHERE email = '$email'";
+        $connection->query($query);
+        echo "alert('error al crear el usuario')";
+}
+$connection->close();
--- front/Perfil/datosPerfil.js
+++ front/Perfil/datosPerfil.js
@@ -3,6 +3,11 @@ let email = paramURL.get('email')
 
 let inputs = document.getElementsByClassName('form-control')
 let btonEditarDatos = document.getElementById('btonEditarDatos')
+let mostrarDatosPago = document.getElementById('mostrarDatosPago')
+let datosPago = document.getElementById('datosPago')
+
+let mostrar = mostrarDatosPago.getAttribute('mostrar')
+
 
 fetch(`../../back/Clientes/php/verCliente.php?email=${email}`)
     .then(response => response.json())
@@ -10,13 +15,13 @@ fetch(`../../back/Clientes/php/verCliente.php?email=${email}`)
         //Parsea la respuesta a JSON
         info = JSON.parse(JSON.stringify(data));
         console.log(info);
-            
-          inputs[0].value = info[0].name
-          inputs[1].value = info[0].surname
-          inputs[2].value = info[0].email
-          inputs[3].value = info[0].fecha_nacimiento
-          inputs[4].value = info[0].weight
-          inputs[5].value = info[0].height
+
+        inputs[0].value = info[0].name
+        inputs[1].value = info[0].surname
+        inputs[2].value = info[0].email
+        inputs[3].value = info[0].fecha_nacimiento
+        inputs[4].value = info[0].weight
+        inputs[5].value = info[0].height
 
         //Llama a la función y crea una tabla con los clientes
     })
@@ -29,4 +34,16 @@ btonEditarDatos.addEventListener('click', () => {
     inputs[2].disabled = false
 })
 
+mostrarDatosPago.addEventListener('click', () => {
+    // Muestra datos de pago
+    if (mostrarDatosPago.getAttribute(mostrar) == "true") {
+        datosPago.style.display = 'none'
+        mostrarDatosPago.innerHTML = "Mostrar datos de pago"
+        mostrarDatosPago.setAttribute(mostrar, "false")
+    } else { // Oculta datos de pago
+        datosPago.style.display = 'block'
+        mostrarDatosPago.innerHTML = "Ocultar datos de pago"
+        mostrarDatosPago.setAttribute(mostrar, "true")
+    }
+})
 
