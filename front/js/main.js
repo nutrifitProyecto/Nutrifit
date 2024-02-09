7fdccdfdcfa97f5229c392a806d618e67f9e7cb6
cesiouvas <sergiocastillorens@gmail.com>
2024-02-09 08:13:53 +0100

    cambio action inicio sesión

--- front/js/main.js
+++ front/js/main.js
@@ -64,6 +64,30 @@ window.addEventListener("load", function () {
   datosSesion()
 });
 
+let logType = document.getElementById('logType')
+
+logType.addEventListener('click', () => {
+  let form = document.getElementById('formLogIn')
+  let attr = form.getAttribute('logTo')
+
+  if (attr == 'cliente') {
+    form.setAttribute('logTo', 'entrenador')
+    logType.innerHTML = 'Soy entrenador'
+    document.getElementById('formLogIn').action = "../../back/src/loginEntrenador.php"
+    console.log(document.getElementById('formLogIn'));
+  } else {
+    form.setAttribute('logTo', 'cliente')
+    logType.innerHTML = 'Soy cliente'
+    document.getElementById('formLogIn').action = "../../back/src/loginClient.php"
+    console.log(document.getElementById('formLogIn'));
+  }
+})
+
+
+
+
+
+
 //Boton de menu en movil
 function displayChange() {
   let bttn = document.querySelector("header nav");
