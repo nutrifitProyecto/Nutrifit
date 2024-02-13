41e2d65fa8c22fae0fe21a22a5d7c36ad50c389c
Aris7ides <joseamaristam@gmail.com>
2024-02-13 13:35:44 +0100

    No esta acabado paginaCursos

---
+++ back/Dietas/php/getDietasbyEnt.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include "../../inc/dbinfo.inc";
+
+//Creación de la conexión
+$connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+$query = 'SELECT
+e.name AS Entrenador,
+d.nombre AS Dieta,
+d.description AS Descripcion
+FROM
+dietas d
+LEFT JOIN
+cursos_dietas cd ON d.id = cd.idDieta
+LEFT JOIN
+cursos c ON cd.idCurso = c.id
+JOIN
+entrenadores e ON c.idEntrenador = e.id';
+
+$result = $connection->query($query);
+
+if($result->num_rows > 0) {
+    echo json_encode($result->fetch_all(MYSQLI_ASSOC));
+}
+
+$connection->close();
--- front/cursos/cursos.js
+++ front/cursos/cursos.js
@@ -1,76 +1,59 @@
-let table = document.getElementById('entrenador-table')
-let info = []
-let inputs = document.querySelectorAll('input')
+let divEntrenamientos=document.getElementById("showAllEntrenamientos");
+let divDietas=document.getElementById("showAllDietas");
 
-limpiarCampos()
+let infoEntrenamientos = [];
 
-fetch('./php/getEntrenador.php')
+fetch('../../back/Entrenamientos/php/getEntrenamientosbyEnt.php')
     .then(response => response.json())
     .then((data) => {
         //Parsea la respuesta a JSON
-        info = JSON.parse(JSON.stringify(data));
-        //Llama a la función y crea una tabla con los entrenadores
-        tablaEntrenadores()
+        infoEntrenamientos = JSON.parse(JSON.stringify(data));
+        //Llama a la función y crea una tabla con los ejercicios
+        insertEntrenamientos();
     })
     .catch(error => {
         console.log(error);
     });
 
-//Crear la tabla con todos los entrenadores
-function tablaEntrenadores() {
-    let cad = `<table class="table table-striped">
-                    <tr>
-                        <th>id</th>
-                        <th>Nombre</th>
-                        <th>Apellido</th>
-                        <th>Email</th>
-                        <th>Contraseña</th>
-                        <th>Descripción</th>
-                        <th>opciones</th>
-                    </tr>`
-    info.forEach(ent => {
-        cad += `<tr id="column${ent.id}" class="overflow-x-scroll">
-                    <td>${ent.id}</td>
-                    <td>${ent.name}</td>
-                    <td>${ent.surname}</td>
-                    <td>${ent.email}</td>
-                    <td>${ent.password}</td>
-                    <td >${ent.description}</td>
-                    <td class="overflow-x-auto">
-                        <button onclick="eliminarEntrenador(${ent.id})" class="btn btn-danger">Eliminar</button>
-                        <button id="btnEditar" data-bs-toggle="modal" data-bs-target="#editarEntrenadorModal" class="btn btn-success" onclick="llenarCampos(${ent.id})">Editar</button>
-                    </td>
-                </tr>`
+//Crear la tabla con todos los ejercicios
+function insertEntrenamientos() {
+    let cad = `<div id="faqs">`;
+    infoEntrenamientos.forEach(ent => {
+        cad+=`
+        <button class="accordion">${ent.Entrenamiento} / ${ent.Entrenador}</button>
+        <div class="panel">
+            <p>${ent.Descripcion}</p>
+        </div>`;
     });
-    cad += `</table>
-            <button data-bs-toggle="modal" data-bs-target="#crearEntrenadorModal" class="btn btn-primary">Crear entrenador</button>`
-    table.innerHTML = cad
+    cad+=`</div>`;
+    divEntrenamientos.innerHTML += cad;
 }
 
-//Elimina un entrenador con respecto al id de este
-function eliminarEntrenador(sendId) {
-    $.ajax({
-        type: "POST", //POST para enviar los datos al php
-        url: "./php/eliminarEntrenador.php",
-        data: { idToDelete: sendId }, // Enviar la variable como parte de los datos
-        success: window.location = "./entrenadorList.html"
+
+let infoDietas = [];
+
+fetch('../../back/Dietas/php/getDietasbyEnt.php')
+    .then(response => response.json())
+    .then((data) => {
+        //Parsea la respuesta a JSON
+        infoDietas = JSON.parse(JSON.stringify(data));
+        //Llama a la función y crea una tabla con los ejercicios
+        insertDietas();
+    })
+    .catch(error => {
+        console.log(error);
     });
-}
 
-//Rellena los campos de la ventana modal
-function llenarCampos(id) {
-    let editInputs = document.getElementsByName('showValues')
-
-    //Asigno valores con respecto a los campos de la tabla
-    editInputs[0].childNodes[1].value = document.getElementById('column' + id).childNodes[1].innerHTML
-    editInputs[1].childNodes[4].value = document.getElementById('column' + id).childNodes[3].innerHTML
-    editInputs[2].childNodes[4].value = document.getElementById('column' + id).childNodes[5].innerHTML
-    editInputs[3].childNodes[4].value = document.getElementById('column' + id).childNodes[11].innerHTML
-}
-
-//Limpia los campos de la ventana modal
-function limpiarCampos() {
-    inputs.forEach(element => {
-        element.value = ""
+//Crear la tabla con todos los ejercicios
+function insertDietas() {
+    let cad = ``;
+    infoDietas.forEach(ent => {
+        cad+=`<div>
+        <h3>${ent.Dieta}</h3>
+        <h4>${ent.Descripcion}</h4>
+        <h5>${ent.Entrenador}</h5>
+        </div>
+        `;
     });
+    divDietas.innerHTML += cad;
 }
